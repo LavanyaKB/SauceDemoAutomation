0b14be1c84d8856e9a4b9d7adef000eff917e171
Lavanya <lavanyakb19@gmail.com>
2024-07-23 11:59:11 +0100

    initial commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"org.saucedemo.stepdefs"},
-        plugin = {"pretty", "html:target/cucumber-reports"}
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 public class TestRunner {
 }
